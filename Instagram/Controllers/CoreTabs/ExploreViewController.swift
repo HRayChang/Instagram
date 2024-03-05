341409429389775051e42c4b5458a5ac3b9b37f6
RayChang <raychang861205@gmail.com>
2024-03-06 00:16:17 +0800

    create MVC group and VC files

---
+++ Instagram/Controllers/CoreTabs/ExploreViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreViewController.swift
+//  Instagram
+//
+//  Created by Ray Chang on 2024/3/5.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
