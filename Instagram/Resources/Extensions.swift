537e888b8e18a40baf1ab567e9aa4fff4e735993
RayChang <raychang861205@gmail.com>
2024-03-06 23:01:09 +0800

    create Extensions file

---
+++ Instagram/Resources/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Instagram
+//
+//  Created by Ray Chang on 2024/3/6.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+}
