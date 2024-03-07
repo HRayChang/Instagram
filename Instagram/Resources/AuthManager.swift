e60c48cb03307fcddf593f5f524f7c4a9f18cd00
RayChang <raychang861205@gmail.com>
2024-03-07 16:20:12 +0800

    setup creat account logic

--- Instagram/Resources/AuthManager.swift
+++ Instagram/Resources/AuthManager.swift
@@ -13,13 +13,31 @@ public class AuthManager {
     
     // MARK: - Public
     
-    public func registerNewUser(username: String, email: String, password: String) {
+    public func registerNewUser(username: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
         /*
          - Check if username is available
          - Check if email is available
-         - Creat account
-         - Insert account to database
          */
+        DatabaseManager.shared.canCreateNewUser(with: email, username: username) { canCreate in
+            if canCreate {
+                /*
+                 - Creat account
+                 - Insert account to database
+                 */
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    guard error == nil, result != nil else {
+                        // Firebase auth could not create account
+                        completion(false)
+                        return
+                    }
+                    
+                    // Insert into database
+                }
+            } else {
+                // Either username or email does not exist
+                completion(false)
+            }
+        }
     }
     
     public func loginUser(username: String?, email: String?, password: String, completion: @escaping (Bool) -> Void) {
