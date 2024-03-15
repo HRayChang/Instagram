626170c5442338dd8bac9d90aacad5e792e038cd
RayChang <raychang861205@gmail.com>
2024-03-16 01:13:26 +0800

    setup basic UI and Storage

--- Instagram/Resources/StorageManager.swift
+++ Instagram/Resources/StorageManager.swift
@@ -11,6 +11,34 @@ public class StorageManager {
     
     static let shared = StorageManager()
     
+    private let bucket = Storage.storage().reference()
+    
+    public enum IGStorageManagerError: Error {
+        case failedToDownload
+    }
+    
     // MARK: - Public
     
+    private func uploadUserPost(model: UserPost, completion: (Result<URL, Error>) -> Void) {
+        
+    }
+    
+    private func downloadImage(with reference: String, completion: @escaping (Result<URL, IGStorageManagerError>) -> Void) {
+        bucket.child(reference).downloadURL(completion: { url, error in
+            guard let url = url, error == nil else {
+                completion(.failure(.failedToDownload))
+                return
+            }
+            
+            completion(.success(url))
+        })
+    }
+}
+
+public enum UserPostType {
+    case photo, video
+}
+
+public struct UserPost {
+    let postType: UserPostType
 }
---
+++ Instagram/Views/Cells/HomeFeed/IGFeedPostGeneralTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  IGFeedPostGeneralTableViewCell.swift
+//  Instagram
+//
+//  Created by Ray Chang on 2024/3/16.
+//
+
+import UIKit
+
+class IGFeedPostGeneralTableViewCell: UITableViewCell {
+
+
+}
