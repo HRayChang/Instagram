7fd58c2afda5eb8dd8560d3b66077c53d7372b23
RayChang <raychang861205@gmail.com>
2024-03-26 18:02:10 +0800

    setup profile collectionView

---
+++ Instagram/Views/Cells/PhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Instagram
+//
+//  Created by Ray Chang on 2024/3/24.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PhotoCollectionViewCell"
+}
---
+++ Instagram/Views/CollectionViewHeaders/ProfileInfoHeaderCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileInfoHeaderCollectionReusableView.swift
+//  Instagram
+//
+//  Created by Ray Chang on 2024/3/24.
+//
+
+import UIKit
+
+class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
+        static let identifier = "ProfileInfoHeaderCollectionReusableView"
+}
